276fc4b3f68b979c5ff980f8095c1a7aef362d5e
Matthew Kim <mkim25@wustl.edu>
2022-10-16 01:02:32 -0500

    transactions api

--- client/API/Transaction.js
+++ client/API/Transaction.js
@@ -1,13 +1,27 @@
-import sampleTransactions from '../SampleTransaction.js'
-const getTransactions= async(userID) => {
-    // transactionIDs are an array of transaction IDs
-    // append userID and username
-    return sampleTransactions
-    if(TransactionIDs==null){
-        return [];
+const getTransactions = async (amount, isCompleted, people) => {
+    
+    var url = "http://localhost:3000/transaction";
+
+    var response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            amount: amount,
+            isCompleted: isCompleted,
+            people: people,
+        }),
+    });
+
+    var data = await response.json();
+    if(data.error!=undefined){
+        alert(data.error);
+        return null
     }
-    else{
-        return sampleTransactions;
-    }
-}
+    console.log(data);
+    return response;
+};
+
 export {getTransactions}
