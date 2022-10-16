6ff81a518acc883136e3e9bb60d812d6535d2dc0
Gunwoo Lee <gunwoo@wustl.edu>
2022-10-16 11:46:02 -0500

    df

---
+++ client/API/SampleTransaction.js
@@ -0,0 +1,36 @@
+const sampleTransactions = [
+  {
+    id: "3452",
+    // name:"U-City-Grill",
+    amount: 23.34,
+    isCompleted: true,
+    people: [
+      { id: "4453", username: "gunwoo" },
+      { id: "323452", username: "matthew" },
+      { id: "323455", username: "Lien" },
+    ],
+  },
+  {
+    id: "342",
+    // name:"U-City-Grill",
+    amount: 23.34,
+    isCompleted: true,
+    people: [
+      { id: "4r3", username: "gunwoo" },
+      { id: "3ertw2", username: "matthew" },
+      { id: "3e5", username: "Lien" },
+    ],
+  },
+  {
+    id: "3454",
+    // name:"U-City-Grill",
+    amount: 23.34,
+    isCompleted: true,
+    people: [
+      { id: "40", username: "gunwoo" },
+      { id: "33", username: "matthew" },
+      { id: "387", username: "Lien" },
+    ],
+  },
+];
+export default sampleTransactions;
